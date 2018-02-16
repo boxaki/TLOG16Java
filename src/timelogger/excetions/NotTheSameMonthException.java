6a006746ac939439b6b0c6782ce7801c8bd11775
Akos Varga <boxakos@gmail.com>
2018-02-16 10:07:48 +0100

    Remove header.

--- src/timelogger/excetions/NotTheSameMonthException.java
+++ src/timelogger/excetions/NotTheSameMonthException.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package timelogger.excetions;
 
 /**
@@ -15,7 +10,7 @@ public class NotTheSameMonthException extends Exception {
      * Creates a new instance of <code>NotTheSameMonthException</code> without
      * detail message.
      */
-    public NotTheSameMonthException() {
+    public NotTheSameMonthException(){
     }
 
     /**
